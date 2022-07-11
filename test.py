130478333e826680da19771a42e57868020704af
Aa_Asteri <81508133+Aa-Asteri@users.noreply.github.com>
2022-07-10 23:23:38 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "I2UxPw29ixxiw4yKUguS4dtcfvBYPnkIKi1Tmw7D"          # 본인 값으로 변경
-secret = "F8RNFqykSjT0JruTDXIFw5LTHGsOf2s47tdHzNaA"          # 본인 값으로 변경
+access = ""          # 본인 값으로 변경
+secret = ""          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-DOGE"))     # KRW-XRP 조회
