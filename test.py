b4fb2c575c6e3d52454f46b77776435095afb1f0
Aa_Asteri <81508133+Aa-Asteri@users.noreply.github.com>
2022-07-10 23:32:48 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = ""          # 본인 값으로 변경
-secret = ""          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-DOGE"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
